fb411088ff51b66776cb8a66dc26d5d3f4212693
Subhasom <subhasom@gmail.com>
2015-01-07 11:57:00 +0530

    updated
    new app mode fixes

--- bin/init.js
+++ bin/init.js
@@ -42,16 +42,9 @@ function startSearch(){var s=document.getElementById('srch'); var hdr=document.g
 function blurSearch(t){if(document.getElementById('srch').value=='' || t){showQsr('',false); if(pastSearch!=''){pastSearch=''; _ldgAct(60); document.getElementById('srch').blur(); dataLoad('data','auth=2&s');}} if(t){document.getElementById('srch').value=''; document.getElementById('clrBtn').blur();}}
 function submitSearch(){pastSearch=document.getElementById('srch').value; _ldgAct(60); document.getElementById('srch').blur();}
 function showQsr(v,s){if(s){document.getElementById('qSrchRes').innerHTML=v; document.getElementById('hdr').classList.add('sact'); document.getElementById('tda').classList.add('sact2');}else{document.getElementById('qSrchRes').innerHTML=''; document.getElementById('hdr').classList.remove('sact'); document.getElementById('tda').classList.remove('sact2');}}
-function viwItem(id){window.open('view/?'+app_cs+'&'+id+'&v');}
+function viwItem(id){window.open('view/?'+app_cs+'&'+id+'&v','','height=550,width=700,location=0');}
 /*adding item*/
-function addNewTgl(){
-	var b=document.getElementById('addNewBtn');
-	if(b.value!='- CANCEL'){if(document.getElementById('des').innerHTML==''){b.value='...'; _ldgAct(60); dataLoad('data','auth=3&s');}else addNewTglDone();}
-	else{b.value='+ ADD'; document.getElementById('des').classList.add('des'); b.blur();}
-}
+function addNewTgl(){var b=document.getElementById('addNewBtn'); if(b.value!='- CANCEL'){if(document.getElementById('des').innerHTML==''){b.value='...'; _ldgAct(60); dataLoad('data','auth=3&s');}else addNewTglDone();}else{b.value='+ ADD'; document.getElementById('des').classList.add('des'); b.blur();}}
 function addNewTglDone(){document.getElementById('des').classList.remove('des'); window.scrollTo(0,0); document.getElementById('addNewBtn').value='- CANCEL';}
 function chkAvl(ele){if(ele.options[ele.selectedIndex].text=='+ ADD NEW'){var flnd=prompt('Enter new name:'); if(flnd){_ldgAct(40); dataLoad('data','auth=4&s&type='+ele.id+'&v='+flnd); var lcc=document.createElement("option"); lcc.setAttribute('value',flnd); lcc.setAttribute('selected','true'); lcc.innerHTML=flnd; ele.appendChild(lcc);}}}
-function userSubmit(){
-	if(!usrSubmitMode){var c=confirm('Are you sure that you want to ADD this item?'); if(c){_ldgAct(80); usrSubmitMode=true;} else return false;}
-	else return false;
-}
+function userSubmit(){if(!usrSubmitMode){var c=confirm('Are you sure that you want to ADD this item?'); if(c){_ldgAct(80); usrSubmitMode=true;} else return false;}else return false;}
